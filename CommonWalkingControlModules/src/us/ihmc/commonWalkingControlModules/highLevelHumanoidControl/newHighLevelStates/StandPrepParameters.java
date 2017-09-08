c6034890c46defe1eb47217818c9efd2fd9eb822
Robert <rgriffin@ihmc.us>
2017-09-08 17:28:59 -0500

    redid the new controller factory to use the new format

--- CommonWalkingControlModules/src/us/ihmc/commonWalkingControlModules/highLevelHumanoidControl/newHighLevelStates/StandPrepParameters.java
+++
@@ -1,8 +0,0 @@
-package us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.newHighLevelStates;
-
-public interface StandPrepParameters
-{
-   double getSetpoint(int jointIndex);
-   double getSetpoint(String jointName);
-
-}
