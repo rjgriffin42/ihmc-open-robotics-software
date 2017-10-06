a4334120498c46a3f04b74e46116e1edccfae2b2
Robert <rgriffin@ihmc.us>
2017-10-05 22:37:43 -0500

    added the necessary fields into the high level parameters, and created those for thor and escher

---
+++ thor/src/main/java/us/ihmc/thor/parameters/ThorHighLevelControllerParameters.java
@@ -0,0 +1,69 @@
+package us.ihmc.thor.parameters;
+
+import us.ihmc.commonWalkingControlModules.configurations.HighLevelControllerParameters;
+import us.ihmc.commonWalkingControlModules.highLevelHumanoidControl.highLevelStates.StandPrepParameters;
+import us.ihmc.humanoidRobotics.communication.packets.dataobjects.HighLevelController;
+import us.ihmc.sensorProcessing.outputData.LowLevelJointControlMode;
+
+public class ThorHighLevelControllerParameters implements HighLevelControllerParameters
+{
+   @Override
+   public StandPrepParameters getStandPrepParameters()
+   {
+      return null;
+   }
+
+   @Override
+   public LowLevelJointControlMode getLowLevelJointControlMode(String joint, HighLevelController state)
+   {
+      return LowLevelJointControlMode.FORCE_CONTROL;
+   }
+
+   @Override
+   public double getLowLevelJointStiffness(String joint)
+   {
+      return 100.0;
+   }
+
+   @Override
+   public double getLowLevelJointDamping(String joint)
+   {
+      return 0.5;
+   }
+
+   @Override
+   public HighLevelController getDefaultInitialControllerState()
+   {
+      return HighLevelController.DO_NOTHING_BEHAVIOR;
+   }
+
+   @Override
+   public HighLevelController getFallbackControllerState()
+   {
+      return HighLevelController.DO_NOTHING_BEHAVIOR;
+   }
+
+   @Override
+   public boolean automaticallyTransitionToWalkingWhenReady()
+   {
+      return false;
+   }
+
+   @Override
+   public double getTimeToMoveInStandPrep()
+   {
+      return 0;
+   }
+
+   @Override
+   public double getMinimumTimeInStandReady()
+   {
+      return 0;
+   }
+
+   @Override
+   public double getTimeInStandTransition()
+   {
+      return 0;
+   }
+}
