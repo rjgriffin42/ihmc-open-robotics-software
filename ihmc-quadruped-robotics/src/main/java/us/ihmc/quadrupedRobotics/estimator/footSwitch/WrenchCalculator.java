9e31a7223a12f82175da8059af345e44a3e7f21e
Robert <rgriffin@ihmc.us>
2019-03-05 22:20:54 -0600

    started modifying the force based touchdown detector to add to the foot switch properly

--- ihmc-quadruped-robotics/src/main/java/us/ihmc/quadrupedRobotics/estimator/footSwitch/WrenchCalculator.java
+++
@@ -1,9 +0,0 @@
-package us.ihmc.quadrupedRobotics.estimator.footSwitch;
-
-import us.ihmc.mecano.spatial.interfaces.WrenchReadOnly;
-
-public interface WrenchCalculator
-{
-   void calculate();
-   WrenchReadOnly getWrench();
-}
