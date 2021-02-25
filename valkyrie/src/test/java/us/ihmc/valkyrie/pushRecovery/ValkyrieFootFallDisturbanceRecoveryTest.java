f9f3343285b413c25f1d364db66823f0456f8a06
Sylvain Bertrand <sbertrand@ihmc.us>
2021-02-25 16:05:32 -0600

    Tweaked the tests and added the test for Atlas.

--- valkyrie/src/test/java/us/ihmc/valkyrie/pushRecovery/ValkyrieFootFallDisturbanceRecoveryTest.java
+++ valkyrie/src/test/java/us/ihmc/valkyrie/pushRecovery/ValkyrieFootFallDisturbanceRecoveryTest.java
@@ -1,5 +1,9 @@
 package us.ihmc.valkyrie.pushRecovery;
 
+import java.util.function.Consumer;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInfo;
 
@@ -14,6 +18,7 @@ import us.ihmc.valkyrie.ValkyrieRobotModel;
 import us.ihmc.valkyrie.configuration.ValkyrieRobotVersion;
 import us.ihmc.valkyrie.parameters.ValkyrieSwingTrajectoryParameters;
 import us.ihmc.valkyrie.parameters.ValkyrieWalkingControllerParameters;
+import us.ihmc.yoVariables.registry.YoRegistry;
 
 public class ValkyrieFootFallDisturbanceRecoveryTest extends HumanoidFootFallDisturbanceRecoveryTest
 {
@@ -27,6 +32,18 @@ public class ValkyrieFootFallDisturbanceRecoveryTest extends HumanoidFootFallDis
          {
             return new ValkyrieWalkingControllerParameters(getJointMap(), getRobotPhysicalProperties(), getTarget())
             {
+               @Override
+               public double getInitialICPErrorToSlowDownTransfer()
+               {
+                  return 0.10;
+               }
+
+               @Override
+               public double getSwingTimeOverrunToInitializeFreeFall()
+               {
+                  return 0.0;
+               }
+
                @Override
                public SwingTrajectoryParameters getSwingTrajectoryParameters()
                {
@@ -57,9 +74,65 @@ public class ValkyrieFootFallDisturbanceRecoveryTest extends HumanoidFootFallDis
       return robotModel;
    }
 
+   @Tag("humanoid-push-recovery-slow")
    @Test
-   public void testBlindWalkOverHole(TestInfo testInfo) throws Exception
+   public void testBlindWalkOverHole_10cm(TestInfo testInfo) throws Exception
    {
-      super.testBlindWalkOverHole(testInfo, 0.6, 0.25);
+      useExperimentalPhysicsEngine();
+      enableOffsetFootstepsHeightWithExecutionError();
+      testBlindWalkOverHole(testInfo, 0.6, 0.25, 0.10);
+   }
+
+   @Tag("humanoid-push-recovery-slow")
+   @Test
+   public void testBlindWalkOverHole_15cm(TestInfo testInfo) throws Exception
+   {
+      useExperimentalPhysicsEngine();
+      enableToeOffInSingleSupport();
+      enableOffsetFootstepsHeightWithExecutionError();
+      testBlindWalkOverHole(testInfo, 0.6, 0.25, 0.15);
+   }
+
+   @Disabled // TODO Falls on the way out of the hole because of a poor CMP positioning. 
+   @Tag("humanoid-push-recovery-slow")
+   @Test
+   public void testBlindWalkOverHole_20cm(TestInfo testInfo) throws Exception
+   {
+      useExperimentalPhysicsEngine();
+      enableToeOffInSingleSupport();
+      enableOffsetFootstepsHeightWithExecutionError();
+      Consumer<YoRegistry> varMutator = rootRegistry -> rootRegistry.findVariable("comHeightFallAccelerationMagnitude").setValueFromDouble(7.0);
+      testBlindWalkOverHole(testInfo, 0.6, 0.25, 0.20, varMutator);
+   }
+
+   @Tag("humanoid-push-recovery-slow")
+   @Test
+   public void testBlindWalkOver_10cm_StepDown(TestInfo testInfo) throws Exception
+   {
+      useExperimentalPhysicsEngine();
+      enableOffsetFootstepsHeightWithExecutionError();
+      super.testBlindWalkOverStepDown(testInfo, 0.6, 0.25, 0.10);
+   }
+
+   @Disabled 
+   @Tag("humanoid-push-recovery-slow")
+   @Test
+   public void testBlindWalkOver_15cm_StepDown(TestInfo testInfo) throws Exception
+   {
+      useExperimentalPhysicsEngine();
+      enableToeOffInSingleSupport();
+      enableOffsetFootstepsHeightWithExecutionError();
+      super.testBlindWalkOverStepDown(testInfo, 0.6, 0.25, 0.15);
+   }
+
+   @Tag("humanoid-push-recovery-slow")
+   @Test
+   public void testBlindWalkOver_20cm_StepDown(TestInfo testInfo) throws Exception
+   {
+      useExperimentalPhysicsEngine();
+      enableToeOffInSingleSupport();
+      enableOffsetFootstepsHeightWithExecutionError();
+      Consumer<YoRegistry> varMutator = rootRegistry -> rootRegistry.findVariable("comHeightFallAccelerationMagnitude").setValueFromDouble(7.0);
+      super.testBlindWalkOverStepDown(testInfo, 0.6, 0.25, 0.20, varMutator);
    }
 }
