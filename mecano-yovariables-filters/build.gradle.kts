eb6b676317b1ae8c61c2857e73e4b99eb5435c5d
rgriffin@ihmc.org <rgriffin@ihmc.us>
2024-10-16 14:01:36 -0500

    cleaned up the ihmc math package for extraction
    prepped the yo variables by moving them into their own packages

--- mecano-yovariables-filters/build.gradle.kts
+++ mecano-yovariables-filters/build.gradle.kts
@@ -7,22 +7,16 @@ plugins {
 
 ihmc {
    loadProductProperties("../product.properties")
-   
+
    configureDependencyResolution()
    configurePublications()
 }
 
 mainDependencies {
-   api("us.ihmc:scs2-definition:17-0.27.3")
-   api("us.ihmc:ihmc-graphics-description:0.25.1")
-   api("us.ihmc:mecano-yovariables:17-0.18.1")
    api("us.ihmc:ihmc-yovariables-filters:source")
-   api("jgraph:jgraph:5.13.0.0")
+   api("us.ihmc:mecano-yovariables:17-0.18.1")
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-commons-testing:0.32.0")
-   api("us.ihmc:ihmc-matrix-library:0.19.0")
-   api("us.ihmc:euclid-test:0.21.0")
-   api("org.apache.commons:commons-math3:3.3")
+   api("us.ihmc:ihmc-yovariables-filters-test:source")
 }
